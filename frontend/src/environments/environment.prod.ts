9907c3642d3a4c619fb2249d283fb525416bc4a2
Sonia Sherif <112129570+soniaSherif@users.noreply.github.com>
2025-12-09 17:33:16 -0600

    Revert "Backend betty"

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -3,7 +3,5 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: true,
-  apiBaseUrl: 'http://localhost:8000/api/',
-  googleMapsKey: ''
-
+   apiBaseUrl: 'http://localhost:8000/api/'
 };

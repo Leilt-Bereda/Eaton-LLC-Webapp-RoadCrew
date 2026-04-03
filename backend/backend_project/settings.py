30aa33a3226c270b760c0dce2be03d1034ec2611
lbereda <leilt.bereda@mnsu.edu>
2026-04-03 13:58:36 -0500

    Added X-Client-Version header on middleware

--- backend/backend_project/settings.py
+++ backend/backend_project/settings.py
@@ -58,6 +58,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'myapp.middleware.MobileClientMiddleware',
+
 ]
 
 ROOT_URLCONF = 'backend_project.urls'
